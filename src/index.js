a391044f73e0643c9d6d1cd4dba4d39bec7c93da
Pavel <60575337+PaulRomanov@users.noreply.github.com>
2020-10-03 16:45:52 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
+exports.min = function min(array) {
 
-exports.min = function min (array) {
-  return 0;
+  if (arguments.length === 0 || array.length === 0) {
+      return 0
+  }
+
+
+  return Math.min.apply(null, array)
+
+
 }
 
-exports.max = function max (array) {
-  return 0;
+
+exports.max = function max(array) {
+
+  if (arguments.length === 0 || array.length === 0) {
+      return 0
+  }
+
+  return Math.max.apply(null, array)
+
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (arguments.length === 0 || array.length === 0) {
+      return 0
+  } else {
+      t = 0;
+      for (i = 0; i < array.length; i++)
+          t += array[i];
+      sum = t / array.length;
+      return sum;
+  }
 }
